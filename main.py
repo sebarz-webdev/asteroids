2c1cdeb021119530f87b0a37237a74cbbbcfd79e
sebarz-webdev <websebarz@proton.me>
2024-12-23 16:57:08 -0600

    add circleshape.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -11,6 +13,9 @@ def main():
             return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
