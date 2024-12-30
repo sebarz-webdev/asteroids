508724abf726b4cd8bfa67c95f76e18b52877877
sebarz-webdev <websebarz@proton.me>
2024-12-30 16:09:18 -0600

    update main with groups in game loop

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -14,10 +21,14 @@ def main():
          if event.type == pygame.QUIT:
             return
          
-        player.update(dt)
-         
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+         
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
